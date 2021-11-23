7f37bf334fa946fbaa791263f739e6051d42bbd2
Brand28 <alejandrobrand28@gmail.com>
2021-11-23 12:00:47 -0500

    mas cosas

--- android studio/app/src/main/java/com/lab10_ecosistemas/MainActivity.java
+++ android studio/app/src/main/java/com/lab10_ecosistemas/MainActivity.java
@@ -2,13 +2,35 @@ package com.lab10_ecosistemas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView titleLogin;
+    private EditText inputUsername;
+    private Button bnEnter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titleLogin = findViewById(R.id.titleLogin);
+        inputUsername = findViewById(R.id.inputUsername);
+        bnEnter = findViewById(R.id.bnEnter);
+
+        bnEnter.setOnClickListener(
+                (v) -> {
+                    Intent i = new Intent(this, ListViewer.class);
+                    startActivity(i);
+                    finish();
+                }
+        );
     }
 }
